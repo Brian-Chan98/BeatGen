079a86247a2a76aaf772e6446d0574fa131fcb7d
SathusanK <sathusan.kan@gmail.com>
2018-02-22 02:02:53 -0500

    Created new modules and added test music

---
+++ src/functions.py
@@ -0,0 +1,31 @@
+"""
+-------------------------------------------------------
+functions.py
+Contains all the functions for BeatGen
+-------------------------------------------------------
+Author:  Sathusan Kandasamy
+ID:      170408870
+Email:   kand8870@mylaurier.ca
+__updated__ = "2018-02-22"
+-------------------------------------------------------
+"""
+#[import statements]
+import aifc, pydub
+
+#[constants]
+
+def sound(sound):
+    """
+    -------------------------------------------------------
+    [function description]
+    -------------------------------------------------------
+    Preconditions:
+        [parameter name - parameter description (parameter type and constraints)]
+    Postconditions:
+        [returns or prints]
+        [return value name - return value description (return value type)] 
+    -------------------------------------------------------
+    """
+    
+    return
+
---
+++ src/initialize.py
@@ -0,0 +1,20 @@
+"""
+-------------------------------------------------------
+initialize.py
+Temporary 'main' module for testing.
+-------------------------------------------------------
+Author:  Sathusan Kandasamy
+ID: 170408870
+Email: kand8870@mylaurier.ca
+__updated__ = "2018-02-22"
+-------------------------------------------------------
+"""
+# Imports
+from functions import sound
+
+# Inputs
+print("This is working")
+# Function calls
+
+#Outputs
+

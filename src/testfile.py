00fff9670d747a23b4f5894e2831079bcb8327d2
SathusanK <sathusan.kan@gmail.com>
2018-02-22 01:17:52 -0500

    initial commit

---
+++ src/testfile.py
@@ -0,0 +1,20 @@
+"""
+-------------------------------------------------------
+testfile.py
+[program description]
+-------------------------------------------------------
+Author:  Sathusan Kandasamy
+ID: 170408870
+Email: kand8870@mylaurier.ca
+__updated__ = "2018-02-22"
+-------------------------------------------------------
+"""
+# Imports
+#from [file] import [function]
+
+# Inputs
+print("This is a test")
+# Function calls
+
+#Outputs
+

a5a6e7979e903f2d137e1040e558e0c3709ab710
Sathusan Kandasamy <kand8870@mylaurier.ca>
2018-02-24 02:11:13 -0500

    Added functions.py via upload
    Re-added functions.py to solve and error.

---
+++ src/functions.py
@@ -0,0 +1,32 @@
+"""
+-------------------------------------------------------
+functions.py
+Contains all the functions for BeatGen
+-------------------------------------------------------
+Author:  Sathusan Kandasamy
+ID:      170408870
+Email:   kand8870@mylaurier.ca
+__updated__ = "2018-02-22"
+-------------------------------------------------------
+"""
+#[import statements]
+import aifc, pydub
+from song import Song
+
+#[constants]
+
+def mixer(song):
+    """
+    -------------------------------------------------------
+    [function description]
+    -------------------------------------------------------
+    Preconditions:
+        [parameter name - parameter description (parameter type and constraints)]
+    Postconditions:
+        [returns or prints]
+        [return value name - return value description (return value type)] 
+    -------------------------------------------------------
+    """
+    
+    return
+

ba2b03c12d387aff5044833e68e79a6f786c7b8f
Sathusan Kandasamy <kand8870@mylaurier.ca>
2018-02-24 02:12:37 -0500

    Add initialize.py via upload
    Re-uploaded initialize.py to fix an error.

---
+++ src/initialize.py
@@ -0,0 +1,22 @@
+"""
+-------------------------------------------------------
+initialize.py
+Temporary 'main' module for testing.
+-------------------------------------------------------
+Author:  Sathusan Kandasamy
+ID: 170408870
+Email: kand8870@mylaurier.ca
+__updated__ = "2018-02-22"
+-------------------------------------------------------
+"""
+# Imports
+from functions import mixer
+from song import Song
+
+# Inputs
+song = Song()
+print("This is working")
+# Function calls
+
+#Outputs
+
